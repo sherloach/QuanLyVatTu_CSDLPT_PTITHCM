1153e40cf324243db248862441246b8f7fba7ccf
Hoa Ngo <52942350+itialan@users.noreply.github.com>
2020-12-11 22:00:52 +0700

    Create SP_CHECKID_TRACUU.sql
    Use site 3 for check id

---
+++ Database/SP_CHECKID_TRACUU.sql
@@ -0,0 +1,26 @@
+CREATE PROCEDURE [dbo].[SP_CHECKID_TRACUU]
+@Code NVARCHAR(30), @Type NVARCHAR(15)
+AS
+BEGIN
+	-- Nhân viên
+	IF(@Type = 'MANV')
+	BEGIN
+		IF EXISTS(SELECT * FROM LINK2.QLVT_DATHANG.dbo.NhanVien AS NV WHERE NV.MANV = @Code)
+			RETURN 1; -- Mã NV tồn tại đã tồn tại
+	END
+
+	-- Kho
+	IF(@Type = 'MAKHO')
+	BEGIN
+		IF EXISTS(SELECT * FROM LINK2.QLVT_DATHANG.dbo.Kho AS KHO WHERE KHO.MAKHO = @Code)
+			RETURN 1; -- Mã Kho đã tồn tại
+	END
+
+	IF(@Type = 'TENKHO')
+	BEGIN
+		IF EXISTS(SELECT * FROM LINK2.QLVT_DATHANG.dbo.Kho AS KHO WHERE KHO.TENKHO = @Code)
+			RETURN 1; -- Tên Kho đã tồn tại
+	END
+
+	RETURN 0 -- Không bị trùng
+END
